3560472ab0210783fc165baa25318c5193719290
ITIXYXYWA <ukv220@gmail.com>
2022-01-29 22:43:30 +0500

    logs

--- scripts/patterns/ProxyBuilder.js
+++ scripts/patterns/ProxyBuilder.js
@@ -4,23 +4,22 @@ class ProxyBuilder {
   constructor () {
     this.Builder = new Builder()
   }
-
   
 	addUrl (url) {
     this.Builder.addUrl(url)
-    console.log(url)
+    console.warn("URL:", url)
 		return this
 	}
 
 	addUri (uri) {
     this.Builder.addUri(uri)
-    console.log(uri)
+    console.warn("URI", uri)
 		return this
 	}
 
 	addMethod (method) {
     this.Builder.addMethod(method)
-    console.log(method)
+    console.warn("METHOD", method)
 		return this
 	}
 
